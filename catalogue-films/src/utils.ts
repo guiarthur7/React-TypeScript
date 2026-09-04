7f391c2f2c0c445e296dcfb2213732b3fb6efb11
guiarthur7 <arthur.guillaume@ynov.com>
2026-09-04 14:59:10 +0200

    fix

--- catalogue-films/src/utils.ts
+++ catalogue-films/src/utils.ts
@@ -77,7 +77,7 @@ export function trierPar<T>(liste: T[], cle: keyof T): T[] {
 // --- 5. Un paramètre optionnel jamais vérifié -------------------------
 // Appelée sans genre, cette fonction filtre sur `undefined`.
 
-export function filtrerParGenre(liste: Film[], genre?: string): Film[] {
+export function filtrerParGenre(liste: Film[], genre?: genres): Film[] {
   if (!genre) return liste;
   return liste.filter((film) => film.genres.includes(genre));
 }
